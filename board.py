8014de55beec6e822aa118c26ac970157881a863
Lajja Pancholy <lajja.pancholy@gmail.com>
2018-07-18 16:02:15 -0400

    adventure game with beginning of tree, point and board classes.

---
+++ board.py
@@ -0,0 +1,5 @@
+class Board:
+    def __init__(self):
+        self.x_max = 8
+        self.y_max= 8
+        self.trees = []
---
+++ tree.py
@@ -0,0 +1,9 @@
+class Tree:
+    def __init__(self, treeType, location): # creates a tree object
+        self.treeType = treeType
+        self.location = location
+
+    def speak(self):
+        # put hint or attack here
+
+    
